9b1edc9cd67db886c6245fe5e8602303bcbec370
Richard ~~ <86895322+afonsonzango@users.noreply.github.com>
2024-06-21 22:21:01 +0100

    first commit

---
+++ src/components/cui/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import "./styles/style.css";
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="main_header">
+        <div className="main-assets"></div>
+        <nav className="main-nav"></nav>
+    </header>
+  )
+}
+
+export default Navbar;
