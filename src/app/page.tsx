85685c12b9bb45642e60ccc174477bccfd570f9d
Richard ~~ <86895322+afonsonzango@users.noreply.github.com>
2024-06-22 01:15:09 +0100

    _

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Home() {
   return (
-    <main>
-      Home page design
+    <main className="container">
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
+      Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
     </main>
   );
 }
--- src/components/cui/navbar/navbar.tsx
+++ src/components/cui/navbar/navbar.tsx
@@ -1,11 +1,112 @@
-import "./styles/style.css";
 import React from 'react'
+import "./styles/style.css";
+import Image from "next/image";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Check, Heart, Mail, Phone, Search, ShoppingCart, X } from "lucide-react";
+
+import logo from "@/assets/midea/logo-icon/logo-jabakule-307x83.png";
 
 const Navbar = () => {
   return (
     <header className="main_header">
-        <div className="main-assets"></div>
-        <nav className="main-nav"></nav>
+      <div className="main-assets">
+        <div className="container">
+          <div className="assets-align-copper">
+            <div className="assets-left">
+              <div className="logo-item">
+                <Image src={logo} alt={"jabakule logo"} />
+              </div>
+            </div>
+
+            <div className="assets-center">
+              <div className="input-component">
+                <div className="icon">
+                  <Search />
+                </div>
+                <input type="text" placeholder="Encontre o que procura..." />
+                <div className="cleaner-copper">
+                  <button className="cursor-pointer">
+                    <X />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="assets-right">
+              <div className="assets-right-copper">
+                <div className="assets-list-item">
+                  <span className="assets-money-emout">893,04 Kz</span>
+                  <button className="menu-list-item">
+                    <ShoppingCart />
+                    <div className="assets-item-counter">99+</div>
+                  </button>
+                </div>
+
+                <div className="assets-list-item">
+                  <button className="menu-list-item">
+                    <Heart />
+                    <div className="assets-item-counter">99+</div>
+                  </button>
+                </div>
+
+                <DropdownMenu>
+                  <DropdownMenuTrigger style={{ outline: "none" }}>
+                    <div className="assets-profile">
+                      <div className="profile-name">Ola, <span>afonso!</span></div>
+                      <div className="image"></div>
+                    </div>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="dropdown-menu-restyle">
+                    <div className="dropdown-container">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet distinctio animi explicabo unde magni adipisci sint numquam assumenda magnam eveniet mollitia, ipsam eius dolorem temporibus tempora impedit sit commodi sunt!
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="main-nav">
+        <div className="container">
+          <div className="main-nav-assets">
+            <div className="nav-left-assets">
+              <div className="nav-left-assets-copper">
+                <div className="selected-category">
+                  <Check />
+                  Informatica
+                </div>
+
+                <div className="nav-actions-list">
+                  <ul>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Produtos</a></li>
+                    <li><a href="">Vendedores</a></li>
+                    <li><a href="">Ver lojas</a></li>
+                    <li><a href="">Industriais</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="nav-right-assets">
+              <div className="nav-contact">
+                <div className="text">afonso1.nzango@gmail.com</div>
+                <div className="icon">
+                  <Mail />
+                </div>
+              </div>
+
+              <div className="nav-contact">
+                <div className="text" style={{marginTop: 4}}><span>+244</span> 937146346</div>
+                <div className="icon">
+                  <Phone />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
