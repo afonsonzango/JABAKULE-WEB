ded8af61ff56678bec451588acd6b053331e7318
Richard ~~ <86895322+afonsonzango@users.noreply.github.com>
2024-06-24 15:09:14 +0100

    _

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -1,17 +1,48 @@
+import "@/assets/styles/pages/main/products/products.css";
+
+import product from "@/assets/midea/products/computer.png";
+import Image from "next/image";
+import { ShoppingCart, TrendingUp } from "lucide-react";
+
 export default function Page() {
     return (
-        <main className="container">
-            <h1>Products</h1>
+        <main className="main-container">
+            <div className="container top-bar-container">
+                <div className="cat-title">Navegue pelos produtos</div>
+                <div className="cat-actions">actions</div>
+            </div>
+            <div className="container mx-auto px-4 py-4 products-container">
+                <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="card-image">
+                                <Image src={product} alt={"Computer product image"} />
+                                <div className="card-discount">14% Disconto</div>
+                                <div className="card-trend">
+                                    Up <TrendingUp />
+                                </div>
+                            </div>
+                            <div className="card-title mb-1">
+                                JABAKULE STORE
+                            </div>
+                            <div className="card-name mb-1">
+                                Computador Dell Latitude 3015
+                            </div>
+                            <div className="card-value mb-2">
+                                <span>49.093,5 AOA </span>
+                                <span className="expired">89.093 AOA</span>
+                            </div>
+                            <div className="card-action">
+                                <button className="card-reserve-btn">
+                                    <ShoppingCart />
+                                    Adicionar ao carrinho
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
-            Home page design Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus quo assumenda vel consectetur ea id enim ad minus perferendis magnam voluptates, sed sint inventore maxime sapiente quibusdam at libero ex?
         </main>
     );
 }
